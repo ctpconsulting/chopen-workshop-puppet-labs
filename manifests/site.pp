d76c645980cd32543b4c5fbc24a4fc235887c817
Thomas Hug <Thomas.Hug@ctp.com>
2014-09-02 12:13:09 +0200

    Lab 02

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,15 @@
-file { '/etc/motd':
-    content => 'Good afternoon, Mr Amer. Everything is going extremely well.',
+node default {
+
+    file { '/etc/motd':
+        content => 'Good day, Mr Amer. Everything is going extremely well.',
+    }
+
 }
 
-file { ['/tmp/puppet', '/tmp/puppet/files']:
-    ensure => directory,
-} ->
-file { '/tmp/puppet/files/test':
-    content => 'This is a test',
-    owner   => 'chopen',
-    mode    => 'u+r-wx'
+node 'ubuntu' inherits default {
+
+    file { '/home/chopen/.bashrc':
+        content => 'PS1="\`if [ \$? = 0 ]; then echo \[\e[33m\]^_^\[\e[0m\]; else echo \[\e[31m\]O_O\[\e[0m\]; fi\`[\u@\h:\w]\\$ "',
+    }
+
 }
