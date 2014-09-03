c9581f43c286852a84a813c8e9c3b6f7d4e87deb
Thomas Hug <Thomas.Hug@ctp.com>
2014-09-03 11:21:30 +0200

    Lab 10

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -8,4 +8,15 @@ class nginx(
     include nginx::service
     include nginx::config
 
+    class { '::mysql::server':
+        root_password => 'chopen_root',
+    }
+
+    mysql::db { 'chopen':
+        user     => 'chopen',
+        password => 'chopen',
+        host     => 'localhost',
+        grant    => ['SELECT', 'UPDATE'],
+    }
+
 }
