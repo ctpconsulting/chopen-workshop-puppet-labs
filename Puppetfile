aff7f3e020168ce6aa5dabaa9298af9d9b179377
Thomas Hug <Thomas.Hug@ctp.com>
2014-09-03 11:49:30 +0200

    Lab 11

---
+++ Puppetfile
@@ -0,0 +1,6 @@
+forge "https://forgeapi.puppetlabs.com"
+
+mod "chopen/nginx", :path => "./modules/nginx"
+mod "puppetlabs/ntp",
+	:git => "https://github.com/puppetlabs/puppetlabs-ntp.git",
+	:ref => "3.1.2"
--- manifests/site.pp
+++ manifests/site.pp
@@ -12,4 +12,9 @@ node 'ubuntu' inherits default {
         version => '1.6.0-1+trusty0'
     }
 
+    class { '::ntp':
+        servers => [ '0.ch.pool.ntp.org', '1.ch.pool.ntp.org', 
+                     '2.ch.pool.ntp.org', '3.ch.pool.ntp.org' ],
+    }
+
 }
